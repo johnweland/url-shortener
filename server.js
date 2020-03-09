e0ee0c1f41e43b35efc62c138166eeeb4f8b0a66
John Weland <jweland@icsolutions.com>
2020-03-09 15:09:27 -0500

    added DELETE route && updated redirect with status codes

--- server.js
+++ server.js
@@ -8,19 +8,20 @@ mongoose.connect(
     process.env.MONGO_URI, 
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        useFindAndModify: false
     }
 );
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: false}));
 app.get('/', async (req, res, next) => {
     const urls = await urlModel.find();
-    res.render('index', {urls: urls});
+    res.render('index', {location: process.env.BASE_URL + '/', urls: urls});
 });
 
 app.post('/shorten', async (req, res, next) => {
     await urlModel.create({ full: req.body.fullUrl});
-    res.redirect('/');
+    res.redirect(201, '/');
 });
 
 app.get('/:url', async (req, res, next) => {
@@ -30,6 +31,12 @@ app.get('/:url', async (req, res, next) => {
     url.save();
 
     res.redirect(url.full);
+});
+
+app.delete('/:url', async (req, res, next) => {
+    const url = await urlModel.findOneAndRemove({short: req.params.url});
+    if (url == null) return res.sendStatus(404);
+    res.redirect(204, '/');
 });
 
 app.listen(process.env.PORT || 5000);
