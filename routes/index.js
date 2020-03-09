a64a6544e25aca434dac9a454fa4147300fc3976
John Weland <jweland@icsolutions.com>
2020-03-09 12:12:14 -0500

    ground work for a rework

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Controller = require("../contollers/index");
-
-// @route GET /:code
-// @desc      Redirect to long/original URL
-router.get('/:code', Controller.default);
-
-module.exports = router;
